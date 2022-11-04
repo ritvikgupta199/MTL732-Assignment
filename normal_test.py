0ea8eeb07cb5dcb978e66e1d9cd9ddea81dd831e
Ritvik Gupta <ritvikgupta199@gmail.com>
2022-11-04 22:09:36 +0530

    Added final ticker list

--- normal_test.py
+++ normal_test.py
@@ -2,16 +2,18 @@ from scipy.stats import chisquare, shapiro
 import pandas as pd
 
 ret_csv = 'data/return.csv'
+ticker_file = 'symbols_final.csv'
 
 def main():
+    fw = open(ticker_file, 'w')
     df = pd.read_csv(ret_csv)
     norm = 0
     for col in df.columns:
         stat, pvalue = shapiro(df[col])
         if pvalue >= 0.05:
             norm += 1
-            print(col, pvalue)
-    print(norm)
-
+            fw.write(col + '\n')
+    print(f"{norm} stocks follow normal distribution")
+    fw.close()
 if __name__=='__main__':
     main()
